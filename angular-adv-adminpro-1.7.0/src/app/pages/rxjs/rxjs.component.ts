7622e46e528cc24b5df9e7ca7c9cdf1b7886c0cb
unknown <retro_io@hotmail.com>
2023-01-31 13:14:57 -0300

    Observable map filter and unsubscribe

--- angular-adv-adminpro-1.7.0/src/app/pages/rxjs/rxjs.component.ts
+++ angular-adv-adminpro-1.7.0/src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators'
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+import { retry, take, map, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-rxjs',
@@ -8,16 +8,46 @@ import { retry } from 'rxjs/operators'
   styles: [
   ]
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  intervalSubs: Subscription;
 
   constructor() {
 
-    this.obtenerPromesa().pipe(retry(1))
-      .subscribe(valor => console.log('Subs', valor),
-        error => console.warn(error),
-        () => console.info('OBS TERMINADO')
-      );
+    /* this.obtenerPromesa().pipe(retry(1))
+       .subscribe(valor => console.log('Subs', valor),
+         error => console.warn(error),
+         () => console.info('OBS TERMINADO')
+       );*/
 
+    //this.obtenerIntervaloMap().subscribe(console.log);
+    //this.obtenerIntervalorFilter().subscribe(console.log);
+    this.intervalSubs = this.obtenerIntervaloDestroy().subscribe(console.log);
+  }
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
+  }
+
+  obtenerIntervaloMap(): Observable<number> {
+    return interval(1000)
+      .pipe( // pipe: Forma de transformar la informacion que fluye en un observable
+        map(valor => valor + 1),
+        take(4));
+  }
+
+  obtenerIntervalorFilter(): Observable<number> {
+    return interval(500)
+      .pipe( // pipe: Forma de transformar la informacion que fluye en un observable
+        take(10),
+        map(valor => valor + 1),
+        filter(valor => valor % 2 === 0));
+  }
+
+  obtenerIntervaloDestroy(): Observable<number> {
+    return interval(500)
+      .pipe( // pipe: Forma de transformar la informacion que fluye en un observable
+        map(valor => valor + 1),
+        filter(valor => valor % 2 === 0));
   }
 
   obtenerPromesa(): Observable<number> {
