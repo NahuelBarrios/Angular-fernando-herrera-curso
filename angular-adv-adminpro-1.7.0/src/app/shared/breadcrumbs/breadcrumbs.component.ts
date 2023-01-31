2b91dd8244e251160c32b744664748902646dc8c
unknown <retro_io@hotmail.com>
2023-01-31 16:36:51 -0300

    Add Breadcrumbs

--- angular-adv-adminpro-1.7.0/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ angular-adv-adminpro-1.7.0/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+  titulo: string;
+  tituloSubs$: Subscription;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.tituloSubs$ = this.getArgumentRoute().subscribe(
+      event => {
+        this.titulo = event.titulo;
+        document.title = `AdminPro - ${this.titulo}`;
+      });;
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentRoute() {
+    return this.router.events
+      .pipe(
+        filter(event => event instanceof ActivationEnd && event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data),
+      )
   }
 
 }
